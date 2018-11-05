f6d891c2d3104c47b8eca1929ce962e659156ad3
Frank Yang <puilp0502@gmail.com>
2018-11-06 00:05:33 +0900

    Write test for TodoItem

--- src/components/tests/TodoItem.test.js
+++ src/components/tests/TodoItem.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoItem from '../TodoItemList';
+import { shallow } from 'enzyme';
+import TodoItem from '../TodoItem';
+import styles from '../TodoItem.module.css';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<TodoItem />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it("doesn't crash when remove is clicked", () => {
+  const todoitem = shallow(<TodoItem/>);
+  todoitem.find("."+styles.remove).simulate('click', { stopPropagation() {} });
+})
