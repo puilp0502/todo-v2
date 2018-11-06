fc19a6a318c9e0079957951192a8618e8ffa9a4a
Frank Yang <puilp0502@gmail.com>
2018-11-06 16:31:13 +0900

    Updates

---
+++ precache-manifest.26d124f62ce41e9b96e7ec2f80d4e94e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc1db78db6fbf3b158f0",
+    "url": "/todo-v2/static/js/runtime~main.dc1db78d.js"
+  },
+  {
+    "revision": "a8066c5104f6960ace6c",
+    "url": "/todo-v2/static/js/main.a8066c51.chunk.js"
+  },
+  {
+    "revision": "a0d2cd56e2495897d2b6",
+    "url": "/todo-v2/static/js/1.a0d2cd56.chunk.js"
+  },
+  {
+    "revision": "a8066c5104f6960ace6c",
+    "url": "/todo-v2/static/css/main.ae9f1851.chunk.css"
+  },
+  {
+    "revision": "3f23d478af5616b840e973410ef6bc52",
+    "url": "/todo-v2/index.html"
+  }
+];
